c71a7888fcb77ce49797f51c0aadbc599a1a572f
woothu <32914662+woothu@users.noreply.github.com>
2020-05-20 09:41:58 +0200

    chore: 1.3.2 release - update dependencies and typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,13 +1,13 @@
 import Vue from 'vue'
 
 export declare class CIconRaw extends Vue {
-  name: string
-  content: [string, Array<string>]
-  size: string
-  customClasses: [string, Array<any>, object]
-  src: string
-  title: string
-  use: string
+  name?: string
+  content?: string | Array<string>
+  size?: string
+  customClasses?: string | Array<any> | object
+  src?: string
+  title?: string
+  use?: string
 }
 
 export declare class CIcon extends CIconRaw {}
